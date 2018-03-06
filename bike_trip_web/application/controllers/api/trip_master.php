0ff23f4a27e86f374510146be3d9541b701c7e94
pathompong <pathompong.bo@up.ac.th>
2018-03-06 10:00:40 +0700

    เพิ่ม Update, Select_by_id

--- bike_trip_web/application/controllers/api/trip_master.php
+++ bike_trip_web/application/controllers/api/trip_master.php
@@ -25,13 +25,61 @@ require(APPPATH.'libraries/REST_Controller.php');
                 //set the response and exit
                 $this->response([
                     'status' => $result['status'],
-                    'trip_code' => $result['trip_cade']
+                    'trip_code' => $result['trip_code']
                 ], REST_Controller::HTTP_OK);
             }else{
                 //set the response and exit
                 $this->response([
                     'status' => FALSE,
-                    'message' => 'No user were found.'
+                    'message' => 'can not add master trip'
+                ], REST_Controller::HTTP_NOT_FOUND);
+            }
+            
+        }
+                function edit_master_trip_post(){
+            $result = $this->trip->update($_POST); 
+                  if($result['status']){
+                //set the response and exit
+                $this->response([
+                    'status' => $result['status'],
+                    'trip_code' => $result['trip_code']
+                ], REST_Controller::HTTP_OK);
+            }else{
+                //set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'can not edit master trip'
+                ], REST_Controller::HTTP_NOT_FOUND);
+            }
+            
+        }
+                 function select_id_master_trip_post(){
+                 
+            $result = $this->trip->select_by_id(null,$_POST['id']); 
+            
+                  if($result){
+                //set the response and exit
+//      array('trip_code','user_name','trip_type_code','trip_rights','trip_date','trip_start','trip_end','trip_name','trip_desc','trip_intro_img','trip_status');
+                   $this->response([
+                         'status' => TRUE,
+                         'message' => 'was get MAster trip.',
+                         'trip_code' => $result[0]['trip_code'],
+                         'user_name' => $result[0]['user_name'],
+                         'trip_type_code' => $result[0]['trip_type_code'],
+                         'trip_rights' => $result[0]['trip_rights'],
+                         'trip_date' => $result[0]['trip_date'],
+                         'trip_start' => $result[0]['trip_start'],
+                         'trip_end' => $result[0]['trip_end'],
+                         'trip_name' => $result[0]['trip_name'],
+                         'trip_desc' => $result[0]['trip_desc'],
+                         'trip_intro_img' => $result[0]['trip_intro_img'],
+                         'trip_status' => $result[0]['trip_status']
+                     ], REST_Controller::HTTP_OK);
+            }else{
+                //set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'can not select_id master trip'
                 ], REST_Controller::HTTP_NOT_FOUND);
             }
             
--- bike_trip_web/application/models/trip.php
+++ bike_trip_web/application/models/trip.php
@@ -5,6 +5,18 @@ class Trip extends CI_Model {
      var $table_name = 'trip_master';
     var $primary_key = 'trip_code';
     var $fields_insert =array('trip_code','user_name','trip_type_code','trip_rights','trip_date','trip_start','trip_end','trip_name','trip_desc','trip_intro_img');
+    var $fields_update =array('trip_type_code','trip_rights','trip_date','trip_start','trip_end','trip_name','trip_desc','trip_intro_img');
+    
+        function select_by_id($field = array(), $id)
+    {
+        $sql = "SELECT " . (empty($field) ? "*" : implode($field, ",")) . ' ';
+        $sql .= "FROM {$this->table_name} ";
+        $sql .= "WHERE {$this->primary_key} = '{$id}' ";
+        $sql .= "AND (active='Y') ";
+        
+        $result = $this->db->query($sql);
+        return $result->result_array();
+    }
     
    function insert($arrayObj){
 //       print_r ($arrayObj);
@@ -16,22 +28,44 @@ class Trip extends CI_Model {
             $sql_value .= "'{$value}', ";
         }
         $sql_fields .= "trip_code,active";
+        $sql_fields .= ",trip_date";
         $trip_code = $this->get_trip_code($arrayObj['trip_type_code']);
         $sql_value .="'".$trip_code."'," . "'Y'";
+        $date = date("Y-m-d");
+        $sql_value .=",'".$date."'";
         $sql = "INSERT INTO {$this->table_name} ({$sql_fields}) VALUES ({$sql_value})";
 //         echo $sql;
          $result = $this->db->query($sql);
-          return array('status'=>$result,'trip_cade'=>$trip_code);
+          return array('status'=>$result,'trip_code'=>$trip_code);
+    }
+
+     function update($arrayObj)
+    {
+//        $this->adodb->debug;
+        $sql = "UPDATE {$this->table_name} SET ";
+        foreach ($arrayObj as $key => $value) {            
+            if($this->primary_key != $key ){//ถ้าไม่ใช่ PK key
+                $sql .= "$key='$value', ";
+            }            
+        }
+        $sql .= "active='Y' ";
+        $sql .= "WHERE {$this->primary_key} = '{$arrayObj[$this->primary_key]}' ";
+//        echo $sql;
+       $result = $this->db->query($sql);
+       return array('status'=>$result,'trip_code'=>$arrayObj[$this->primary_key]);
     }
     function get_trip_code($ty_code){
 //        $arrayObj['trip_type_code']=1;
-        $date = date("Ymd");
+        $date = date("Y-m-d");
         $this->db->select('trip_code');
+        $this->db->where("trip_date = '".$date."'");
         $row = $this->db->get('trip_master');
         $amount_row= $row->num_rows()+1;
         $y=substr($date, 2, 2);
-        $m=substr($date, 4, 2);
-        $d=substr($date, 6, 2);
+        $m=substr($date, 5, 2);
+        $d=substr($date,8, 2);
+        $amount_row = sprintf("%03d",$amount_row);
+        $ty_code = sprintf("%02d",$ty_code);
         $trip_code = "$ty_code"."$y"."$m"."$d"."$amount_row";
         return $trip_code;
     }
